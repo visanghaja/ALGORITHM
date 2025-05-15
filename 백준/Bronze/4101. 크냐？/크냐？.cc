b32cfb440ee78ee5e4920d49906e926526528d29
taeh000n <james1208@mju.ac.kr>
2025-05-15 22:48:23 +0900

    [Bronze V] Title: 크냐?, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/4101. 크냐？/크냐？.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    while (true)
+    {
+        int a, b;
+        cin >> a >> b;
+        if (a == 0 && b == 0)
+        {
+            break;
+        }
+        else{
+            if (a > b)
+            {
+                cout << "Yes\n";
+            }
+            else{
+                cout << "No\n";
+            }
+            
+        }
+        
+    }
+    
+
+    return 0;
+}
