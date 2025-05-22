fecee6a6df18a5696ec5c60aae56b6d7bb0b0dd9
taeh000n <james1208@mju.ac.kr>
2025-05-22 21:58:27 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+  int n;
+  string s;
+  cin >> n;
+
+  bool flag;
+  int ans = 0;
+  while(n--) {
+    cin >> s;
+
+    bool isUsed[26] = { false, };
+    isUsed[s[0] - 'a'] = true;
+    flag = true;
+
+    for(int i = 1; i < s.length(); i++) {
+      if(s[i] == s[i - 1]) continue;
+
+      if(isUsed[s[i] - 'a']) flag = false;
+
+      isUsed[s[i] - 'a'] = true;
+    }
+
+    if(flag) ans++;
+  }
+
+  cout << ans;
+
+  return 0;
+}
+ 
