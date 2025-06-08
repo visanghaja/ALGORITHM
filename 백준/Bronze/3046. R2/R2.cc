b06d93590356f43511554ec0733b98b62bb98e57
taeh000n <james1208@mju.ac.kr>
2025-06-08 22:30:59 +0900

    [Bronze IV] Title: R2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/3046. R2/R2.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int r, s;
+    cin >> r >> s;
+    int result;
+    result = (2*s) - r;
+    cout << result;
+    
+    return 0;
+    
+}
