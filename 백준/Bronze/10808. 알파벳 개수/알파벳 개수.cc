86d89357020e2894cfcc171ac480f37b7ddf0bba
taeh000n <james1208@mju.ac.kr>
2025-06-13 21:57:02 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.cc
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main() 
+{
+	string S;
+	cin >> S;
+
+	int alphabet[26] = {};
+
+	for (int i = 0; i < S.length(); i++)
+	{ alphabet[int(S[i])-97]++; }
+
+	for (int num : alphabet)
+	{ cout << num << " "; }
+}
